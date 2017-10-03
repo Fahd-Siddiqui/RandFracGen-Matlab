35c1287061abf87fcc42d59f4f13a08c6a472200
Fahd Siddiqui <drfahdsiddiqui@gmail.com>
2017-10-03 17:19:32 -0500

    Add files via upload

---
+++ @RandGenFracture/RandGenFracture.m
@@ -0,0 +1,15 @@
+classdef RandGenFracture < handle
+    %FRACTURE Class
+    
+    properties
+        Ne=0
+        Fn=0
+        loc
+        locR
+    end
+    
+    methods
+    end
+    
+end
+
