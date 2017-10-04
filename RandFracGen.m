45fc77ee01722f5cc644244433c97e27f2ecc363
Fahd Siddiqui <DrFahdSiddiqui@gmail.com>
2017-10-03 19:26:46 -0500

    Initial commit

--- RandFracGen.m
+++ RandFracGen.m
@@ -1,23 +1,47 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%                        RANDOM FRACTURE GENERATOR                            %
+%                               Version 1.0                                   %
+%                  Written for MATLAB by : Fahd Siddiqui                      %
+%           https://github.com/DrFahdSiddiqui/RandFracGen-Matlab              %
+%                                                                             %
+% =========================================================================== %
+% LICENSE: MOZILLA 2.0                                                        %
+%   This Source Code Form is subject to the terms of the Mozilla Public       %
+%   License, v. 2.0. If a copy of the MPL was not distributed with this       %
+%   file, You can obtain one at http://mozilla.org/MPL/2.0/.                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [locationR, Dom]=RandFracGen(N, Mx, My, theta, tun, plot)
 % Function to generate randomly located fractures
-% Generates random fractures
-% Rotates them
-% Selects the rotated fractures
-% Ensures fractures of size 0 and size 1 elements are removed
-% Rescales the problem to a given size
-% Returns the location matrix with [X_Beg Y_Beg X_End Y_End Fractre_Number]
-% N, Mx, My and tun are used to control the randomness and size of elements
-% Size of final domain
-%   Mx: X size of final domain
-%   My: Y size of final domain
-%   theta: Angle of rotation +ve values only
-%   N: Size of unrotated Domain
-%   tun: Distribution of fractures 9 for large fractures 2 for small fractures
-%   plot: set to yes for plotting
-% Calling example: [locationR, Dom]=RandFracGen(500,5,5,-pi/12,5,0);
+%
+% INPUT
+%   Mx (Integer)    : X size of final domain
+%   My (Integer)    : Y size of final domain
+%   theta (Radian)  : Angle of rotation +ve values only
+%   N (Integer)     : Size of unrotated Domain
+%                     Controls orthogonal spacing between fractures
+%   tun (Integer)   : Size of fracture. 2 for small and 9 for large fractures
+%                     Controls spacing between fracture tips
+%   plot (0 or 1)   : Visualization, 1 for plotting, 0 for no plots
+%
+% STEPS
+%   Generates random fractures in the domain of the size NxN
+%   Rotates the domain and fractures by theta
+%   Selects the rotated fractures
+%   Removes fractures of size 0 and size 1
+%   Rescales the problem to Mx by My size
+%
+% OUTPUT
+%   LocationR : Fracture location matrix with the syntax
+%               [X_Beg Y_Beg X_End Y_End Fractre_Number]
+%   Dom : Class object containing fracture class and elements in each fracture.
+%
+% CALLING EXAMPLE
+%  [locationR, Dom]=RandFracGen(200,5,5,pi/12,5,0);
 
-
-%% Random Generation
+%% Random Generation -------------------------------------------------------- %
 Tiles=true(N+2*tun,N+2*tun,1);
 tic
 Tiles(1,:)=randi(10,N+2*tun,1)>tun ;
@@ -25,7 +49,8 @@ for i=2:size(Tiles,1)
     Tiles(i,:)=randi(10,size(Tiles,1),1)>tun;
     
     for j=1:size(Tiles,2)
-        if (j>2&&j<size(Tiles,2))&&((Tiles(i,j)==Tiles(i-1,j)||Tiles(i,j)==Tiles(i-1,j-1)||Tiles(i,j)==Tiles(i-1,j+1))&&Tiles(i,j)==false)
+        if (j>2&&j<size(Tiles,2))&&((Tiles(i,j)==Tiles(i-1,j)||Tiles(i,j)== ...
+                Tiles(i-1,j-1)||Tiles(i,j)==Tiles(i-1,j+1))&&Tiles(i,j)==false)
             Tiles(i,j)=~Tiles(i,j);
         end
     end
@@ -34,8 +59,7 @@ RandGenTime=toc
 Tiles=~Tiles(tun:end-tun,tun:end-tun);
 
 
-%% Remove Fractures of size 1 element
-
+%% Remove Fractures of size 1 element --------------------------------------- %
 for i=1:size(Tiles,1)
     for j=2:size(Tiles,2)-1
         if (Tiles(i,j-1)==true)&&(Tiles(i,j+1)==true)
@@ -46,7 +70,7 @@ end
 % imshow(~Tiles);
 
 
-%% Find Coordinates of beginning and ending
+%% Find Coordinates of beginning and ending --------------------------------- %
 tic
 CBeg=[];
 CEnd=[];
@@ -67,24 +91,24 @@ for i=1:size(Tiles,1)
 end
 CoordTime=toc
 
-B=[CBeg, CEnd];
+Coord=[CBeg, CEnd];
 
 % Remove size 0 elements
 i=1;
-while i<=size(B,1)
+while i<=size(Coord,1)
     
-    if norm(B(i,1:2)-B(i,3:4))==0||norm(B(i,1:2)-B(i,3:4))==1
-        B(i,:)=[];
+    if norm(Coord(i,1:2)-Coord(i,3:4))==0||norm(Coord(i,1:2)-Coord(i,3:4))==1
+        Coord(i,:)=[];
         i=i-1;
     end
     i=i+1;
 end
 
 
-%% Making the required format
+%% Making the required format ----------------------------------------------- %
 tic
 Dom=RandGenDomain;
-Dom.CreateFractureElements(B);
+Dom.CreateFractureElements(Coord);
 
 % Rotation Matrix
 R=[cos(theta) sin(theta);-sin(theta) cos(theta)];
@@ -105,7 +129,11 @@ s=(N+1)/(abs(sin(theta))+cos(theta));
 
 r=abs((s)/sqrt(2));
 rshift=r/sqrt(2);
-BOX=[MPR(1)-rshift,MPR(2)-rshift;MPR(1)-rshift,MPR(2)+rshift;MPR(1)+rshift,MPR(2)+rshift;MPR(1)+rshift,MPR(2)-rshift;MPR(1)-rshift,MPR(2)-rshift];
+BOX=[MPR(1)-rshift,MPR(2)-rshift;
+    MPR(1)-rshift,MPR(2)+rshift;
+    MPR(1)+rshift,MPR(2)+rshift;
+    MPR(1)+rshift,MPR(2)-rshift;
+    MPR(1)-rshift,MPR(2)-rshift];
 
 if plot==1
     figure
@@ -129,13 +157,19 @@ x_max=max(BOX(:,1));
 
 i=1;
 while i<=Dom.Nf
-    lx_min=double(Dom.Frac(i).locR(:,2)>x_min).*double(Dom.Frac(i).locR(:,2)>x_min);
-    lx_max=double(Dom.Frac(i).locR(:,2)<x_max).*double(Dom.Frac(i).locR(:,2)<x_max);
-    ly_min=double(Dom.Frac(i).locR(:,1)>y_min).*double(Dom.Frac(i).locR(:,3)>y_min);
-    ly_max=double(Dom.Frac(i).locR(:,1)<y_max).*double(Dom.Frac(i).locR(:,3)<y_max);
+    lx_min=double(Dom.Frac(i).locR(:,2)>x_min).* ...
+                                           double(Dom.Frac(i).locR(:,2)>x_min);
+    lx_max=double(Dom.Frac(i).locR(:,2)<x_max).* ...
+                                           double(Dom.Frac(i).locR(:,2)<x_max);
+    ly_min=double(Dom.Frac(i).locR(:,1)>y_min).* ...
+                                           double(Dom.Frac(i).locR(:,3)>y_min);
+    ly_max=double(Dom.Frac(i).locR(:,1)<y_max).* ...
+                                           double(Dom.Frac(i).locR(:,3)<y_max);
     F=lx_min.*lx_max.*ly_min.*ly_max;
-    
-    Dom.Frac(i).locR=Dom.Frac(i).locR.*F;
+    Dom.Frac(i).locR(:,1)=Dom.Frac(i).locR(:,1).*F;
+    Dom.Frac(i).locR(:,2)=Dom.Frac(i).locR(:,2).*F;
+    Dom.Frac(i).locR(:,3)=Dom.Frac(i).locR(:,3).*F;
+    Dom.Frac(i).locR(:,4)=Dom.Frac(i).locR(:,4).*F;
     
     % Remove zeroed out elements
     j=1;
@@ -162,7 +196,7 @@ end
 MakeTime=toc
 
 
-%% Rescaling
+%% Rescaling ---------------------------------------------------------------- %
 for i=1:Dom.Nf
     % Switching X and Y
     temp=Dom.Frac(i).locR(:,1) ;
@@ -182,8 +216,7 @@ end
 [locationR]=Dom.GenerateInputLocationR();
 
 
-%% Plotting
-
+%% Plotting ----------------------------------------------------------------- %
 if plot==1
     figure
     for i=1:Dom.Nf
@@ -195,8 +228,4 @@ if plot==1
 end
 
 
-
-
-
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
